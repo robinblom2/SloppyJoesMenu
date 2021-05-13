4e5604046730f92b823c59266c109007441ae019
Robin Blom <robin.blom@hotmail.se>
2021-05-13 10:17:11 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SloppyJoe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MakeTheMenu();                                  // This method gets called as soon as the program starts.
+        }
+
+        private void MakeTheMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[5];                                                                                         // We create an array of 5 references to the MenuItem-class. 
+            string guacamolePrice;                                                                                                          // We declare this variable that later will contain the randomized price to add guacamole to the order.
+
+
+            for (int i = 0; i < 5; i++)                                                                                                     // The loop iterates 5 times (as many times as dishes displayed on the menu)
+            {
+                menuItems[i] = new MenuItem();                                                                                              // For every iteration of the loop a new index/reference gets pointed at an object of the MenuItems-class.
+
+                if (i >= 3)                                                                                                                 // On iteration 3 & 4 of the loop...
+                {
+                    menuItems[i].Breads = new string[] { "plain bagel", "onion bagel", "pumpernickel bagel", "everything bagel" };          // Add these 4 strings to the field Breads instad of the "original" ones.
+                }
+                menuItems[i].Generate();                                                                                                    // When the current index in the "menuItems"-array have been instatiated and points to the object and its fields, we call on the Generate()-method. This method generates the random-dishes that gets displayed on the menu.
+            }
+
+            // After all the indexes in the "menuItems"-array have received a random dish thru the Generate()-method, we do this:
+
+            item1.Text = menuItems[0].Description;                  // We want to add the Description-field of index 0 (in the array "menuItems") to the first item-TextBlock. 
+            price1.Text = menuItems[0].Price;                       // We want to add the Price-field of index 0 (in the array "menuItems") to the first price-TextBlock. 
+            item2.Text = menuItems[1].Description;                  // And so on...
+            price2.Text = menuItems[1].Price;
+            item3.Text = menuItems[2].Description;
+            price3.Text = menuItems[2].Price;
+            item4.Text = menuItems[3].Description;
+            price4.Text = menuItems[3].Price;
+            item5.Text = menuItems[4].Description;
+            price5.Text = menuItems[4].Price;
+
+            MenuItem specialMenuItem = new MenuItem()               // The last item on the menu is for the daily special. We create an instance of the class MenuItem and call this instance "specialMenuItem"...
+            {
+                Proteins = new string[] { "Organic ham", "Mushroom patty", "Mortadella" },          // We set it's fields Proteins, Breads and Condiments to have special ingredients. 
+                Breads = new string[] { "a gluten free roll", "a wrap", "pita" },
+                Condiments = new string[] { "dijon mustard", "miso dressing", "au jus" }
+            };
+            specialMenuItem.Generate();                                                     // We then call the Generate()-method on the object and the method then randomizes the ingredients above. The result will be stored in the "Description" and "Price"-fields yet again. 
+
+            item6.Text = specialMenuItem.Description;                                       // We want to add the "Description"-field of "specialMenuItem" to the last item-textBlock. 
+            price6.Text = specialMenuItem.Price;                                            // We want to add the "Price"-field of "specialMenuItem" to the last price-textBlock. 
+
+            MenuItem guacamoleMenuItem = new MenuItem();                                    // We create an instance of the class MenuItem and call this instance "guacamoleMenuItem". 
+            guacamoleMenuItem.Generate();                                                   // We then call the Generate()-method on the object and the method generates a random price and stores it in the objects field called "Price". 
+            guacamolePrice = guacamoleMenuItem.Price;
+
+            guacamole.Text = "Add guacamole for " + guacamoleMenuItem.Price;                // We want to add this concatenated string containing the random price to the guacamole-TextBlock.
+        }
+    }
+}
---
+++ MenuItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SloppyJoe
+{
+    class MenuItem
+    {
+        public static Random Randomizer = new Random();                 // Random-klassvariabel. We set this field to static, so that all of the MenuItems share the same single Random Instance. Otherwise the menuitems would be the same on the menu. 
+
+        public string[] Proteins = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };                                     // Klass-array.
+        public string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };       // Klass-array.
+        public string[] Breads = { "rye", "white", "wheat", "pumpernickel", "a roll" };                                                 // Klass-array.
+
+        public string Description = "";                         // This declared field will later contain the full string of a dish displayed on the menu. 
+        public string Price;
+
+        /// <summary>
+        /// This method generates a random Protein, Condiment and Bread and stores the result in Description. It also generates a random price for the dish. 
+        /// </summary>
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];                      // We want to store a random index between 0-5 from the Proteins-array, inside "randomProtein". 
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];                // We want to store a random index between 0-5 from the Condiments-array, inside "randomCondiment". 
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];                            // We want to store a random index between 0-4 from the Breads-array, inside "randomBread". 
+
+            Description = randomProtein + " with " + randomCondiment + " on " + randomBread;        // We concatinate a string containing the full dish, and stores the result in the field "Description".
+
+            decimal bucks = Randomizer.Next(2, 5);                                                  // We want to store a random number between 2-4 inside the variable "bucks". 
+            decimal cents = Randomizer.Next(1, 98);                                                 // We want to store a random number between 1-98 inside the variable "cents". 
+            decimal price = bucks + (cents * .01M);                                                 // First the cents get calculated, then we add the cents to the bucks. The result gets stored in the variable "price". 
+            Price = price.ToString("c");                                                            // We want to convert the decimal-variable to string. We want the price to be displayed as the regional currency (kr). We want to store the result in the field named "Price".  
+
+        }
+    }
+}
